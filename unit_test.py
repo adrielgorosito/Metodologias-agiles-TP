11d1e6fc99fef8ed6421527f32aad0bf86ece279
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-08-07 16:31:56 -0300

    test

--- unit_test.py
+++ unit_test.py
@@ -6,23 +6,23 @@ class Calc():
 
 class CalcTest(unittest.TestCase):
 
-  def test_args_cero_rta_cero(self):
-    
-    calculadora = Calc()
+    def setUp(self):
+        self.calculadora = Calc()
 
-    self.assertEqual(calculadora.sum(0, 0), 0)
+    def test_sum_of_two_zeros_returns_zero(self):
+        self.assertEqual(self.calculadora.sum(0, 0), 0)
 
-  def test_args_dos_rta_cuatro(self):
-    
-    calculadora = Calc()
+    def test_sum_two_and_two_returns_four(self):
+        self.assertEqual(self.calculadora.sum(2, 2), 4)
 
-    self.assertEqual(calculadora.sum(2, 2), 4)
+    def test_sum_two_and_three_returns_five(self):
+        self.assertEqual(self.calculadora.sum(2, 3), 5)
 
-  def test_args_dos_tres_rta_cinco(self):
-    
-    calculadora = Calc()
+    def test_sum_negative_numbers_returns_correct_result(self):
+        self.assertEqual(self.calculadora.sum(-2, -3), -5)
 
-    self.assertEqual(calculadora.sum(2, 3), 5)
+    def test_sum_large_numbers_returns_correct_result(self):
+        self.assertEqual(self.calculadora.sum(1000000, 2000000), 3000000)
 
 if __name__ == '__main__':
     unittest.main()
