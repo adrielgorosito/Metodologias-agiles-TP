a7fb13a96c11e59c683d14ab502afa1b68930449
Adriel Gorosito <adrielgorosito14@gmail.com>
2024-02-10 20:09:20 -0300

    Second acceptance test: guess correct letter

--- tp/features/at.feature
+++ tp/features/at.feature
@@ -4,6 +4,10 @@ Feature: Verificar funcionalidades de la aplicación web
     Given requerimientos
 
   Scenario: Iniciar partida
-    When ingreso la palabra "elefante" en el campo de búsqueda
-    And hago clic en el botón de enviar
+    Given ingreso la palabra "elefante"
     Then debería ver la palabra con la misma cantidad de guiones bajos que letras
+
+  Scenario: Ingresar letra válida
+    Given ingreso la palabra "elefante"
+    When intento la letra correcta "e"
+    Then debería ver la letra revelada en la palabra
--- tp/features/steps/steps.py
+++ tp/features/steps/steps.py
@@ -4,33 +4,47 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# import time
-# time.sleep(5)  5 segundos
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import time
+
+
 @given('requerimientos')
-def given_estoy_en_pagina_inicio(context):
+def requerimientos(context):
     context.driver = webdriver.Chrome()
     context.host = os.getenv('FE_PROJECT_URL')
     context.driver.get(context.host)
 
-@when('ingreso la palabra "{word}" en el campo de búsqueda')
+@given('ingreso la palabra "{word}"')
 def ingresar_palabra(context, word):
     input = context.driver.find_element(By.NAME, "palabra-input")
     input.send_keys(word)
     context.palabra = word
-
-@when('hago clic en el botón de enviar')
-def click_enviar(context):
     submit_button = context.driver.find_element(By.TAG_NAME, "button")
     submit_button.click()
 
 @then('debería ver la palabra con la misma cantidad de guiones bajos que letras')
 def obtener_cantidad_guiones(context):
-    WebDriverWait(context.driver, 5).until(EC.presence_of_element_located((By.ID, "palabraConGuionesBajo")))
-    underscores = context.driver.find_element(By.ID, "palabraConGuionesBajo")
+    WebDriverWait(context.driver, 5).until(EC.presence_of_element_located((By.NAME, "palabra-con-guiones")))
+    underscores = context.driver.find_element(By.NAME, "palabra-con-guiones")
     value_of_underscores = underscores.get_attribute("value").replace(" ", "")
     assert len(value_of_underscores) == len(context.palabra), "Error, la cantidad de guiones bajos no coinciden"
+
+
+@when('intento la letra correcta "{letter}"')
+def intentar_adivinar_letra(context, letter):
+    WebDriverWait(context.driver, 5).until(EC.presence_of_element_located((By.NAME, "letra-input")))
+    input_letter = context.driver.find_element(By.NAME, "letra-input")
+    input_letter.send_keys(letter)
+    context.letter = letter
+    submit_button = context.driver.find_element(By.NAME, "adivinar-letra")
+    submit_button.click()
+
+@then('debería ver la letra revelada en la palabra')
+def verificar_letra_revelada(context):
+    WebDriverWait(context.driver, 5).until(EC.presence_of_element_located((By.NAME, "palabra-con-guiones")))
+    underscores = context.driver.find_element(By.NAME, "palabra-con-guiones")
+    word = underscores.get_attribute("value").replace(" ", "")
+    assert context.letter in word, f"Error, la letra correcta no está en la palabra"
